daff2e633a3bcb0941869854e4d22fad7dc727c0
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-04 17:25:12 -0800

    test: log object

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ if('serviceWorker' in navigator) {
   console.log('browser can use sw');
   navigator.serviceWorker.register('sw.js').then(registration => {
     console.log('registered sw',registration);
+    console.log(brainRegionsObject);
   })
   .catch(error => {
     console.log('boo error',error);
   });
 }
 
-console.log(brainRegionsObject);
+
 
 
 

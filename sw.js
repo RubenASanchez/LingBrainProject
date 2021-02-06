7c5ebcdb9225cdd49c68e7e165bb620cbce7800a
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-06 12:43:53 -0800

    sw: add cache

--- sw.js
+++ sw.js
@@ -4,14 +4,58 @@ const counts = {
   fetches: 0
 };
 
-self.addEventListener('install',() => {
+const CURRENT_CACHE = 'cache-v-1';
+
+const cacheFiles = [
+  './index.html',
+  './css/main.css'
+]
+
+
+self.addEventListener('install', event => {
   console.log('installing sw',++counts.installs);
+  event.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(cacheFiles)
+    })
+  );
 });
 
-self.addEventListener('activate',() => {
+self.addEventListener('activate', event => {
   console.log('activating sw',++counts.activations);
+  event.waitUntil(
+    caches.keys().then(cacheVersions => Promise.all(cacheVersions.filter(cacheVersion => {
+      return cacheVersion != CURRENT_CACHE
+    }).map(cacheVersion => caches.delete(cacheVersion))))
+  );
 });
 
-self.addEventListener('fetch',() => {
+self.addEventListener('fetch', event => {
   console.log('fetching sw',++counts.fetches);
+  event.respondWith(
+    caches.match(event.request).then(async function (cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      try {
+        let freshResponse = await fetch(event.request);
+        if (freshResponse) {
+          return freshResponse;
+        }
+      }
+      catch (error) {
+        var offlinePageResponse = new Response(
+          '',
+          {
+            status: 302,
+            statusText: 'found',
+            headers: {
+              Location: '/offline.html'
+            }
+          }
+        )
+        return offlinePageResponse;
+      }
+    })
+  )
 });

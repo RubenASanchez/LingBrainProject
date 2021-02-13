57179835ad7a6a77ac3806336de2b4ab3f7c1d34
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-13 13:30:26 -0800

    settings: update component names

--- components/settings.js
+++ components/settings.js
@@ -1,7 +1,7 @@
-let spin = document.getElementById('settings-button').addEventListener('click',function() {
-  console.log('clicked me');
+let activateSettings = document.getElementById('settings-button').addEventListener('click',function() {
+  console.log('clicked settings');
   document.getElementsByClassName('settings-icon')[0].classList.add('settings-active');
   console.log('added settings active');
 })
 
-export { spin };
+export { activateSettings };

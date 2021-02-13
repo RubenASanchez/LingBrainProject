e07de8270e34ddd701986f1cb18f00ed59556192
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-13 12:08:15 -0800

    settings: test keyframes

--- components/settings.js
+++ components/settings.js
@@ -1,7 +1,7 @@
 let spin = document.getElementById('settings-button').addEventListener('click',function() {
   console.log('clicked me');
   document.getElementsByClassName('settings-icon')[0].classList.add('settings-active');
-  console.log('added animation keyframes 50%');
+  console.log('added animation keyframes ');
 })
 
 export { spin };

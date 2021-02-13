fd8b10046aabe543541dba0f36c17d1ec689e184
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-13 11:09:43 -0800

    settings: test click

--- components/settings.js
+++ components/settings.js
@@ -1,6 +1,7 @@
 let spin = document.getElementById('settings-button').addEventListener('click',function() {
   console.log('clicked me');
   document.getElementById('settings-icon').classList.add('settings-active');
+  console.log('added active class');
 })
 
 export { spin };

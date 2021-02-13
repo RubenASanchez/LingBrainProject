d7c860ec5fb87e8eaf55e3e806563c2356c54d3f
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-13 11:26:30 -0800

    settings: add class to class

--- components/settings.js
+++ components/settings.js
@@ -1,7 +1,7 @@
 let spin = document.getElementById('settings-button').addEventListener('click',function() {
   console.log('clicked me');
-  document.getElementById('settings-icon').classList.add('settings-active');
-  console.log('added active class');
+  document.getElementsByClassName('settings-icon').classList.add('settings-active');
+  console.log('added active class to class');
 })
 
 export { spin };

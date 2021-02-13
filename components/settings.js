0e6d45fd6fc7a9b87a96041eb2cfc9d10fdd7b70
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-13 13:36:33 -0800

    settings: add settings state display

--- components/settings.js
+++ components/settings.js
@@ -2,6 +2,7 @@ let activateSettings = document.getElementById('settings-button').addEventListen
   console.log('clicked settings');
   document.getElementsByClassName('settings-icon')[0].classList.add('settings-active');
   console.log('added settings active');
+  document.getElementById('current-state').textContent = "settings";
 })
 
 export { activateSettings };

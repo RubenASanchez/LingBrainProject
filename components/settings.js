bf86c9c7ed108c6daae458ba249ed7d4bca6754e
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-13 11:36:12 -0800

    settings: add index to get element

--- components/settings.js
+++ components/settings.js
@@ -1,6 +1,6 @@
 let spin = document.getElementById('settings-button').addEventListener('click',function() {
   console.log('clicked me');
-  document.getElementsByClassName('settings-icon').classList.add('settings-active');
+  document.getElementsByClassName('settings-icon')[0].classList.add('settings-active');
   console.log('added active class to class');
 })
 

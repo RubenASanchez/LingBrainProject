6f9d24073c2eb2a15a98f585965b3a5e8b95b602
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-21 09:48:41 -0800

    settings: refactor activate button

--- components/settings.js
+++ components/settings.js
@@ -1,13 +1,13 @@
-let currentState = document.getElementById('current-state')
+let currentState = document.getElementById('current-state');
+let settingsButton = document.getElementById('settings-button')
 
-let activateSettings = document.getElementById('settings-button').addEventListener('click',function() {
+let activateSettings = settingsButton.addEventListener('click',function() {
   console.log('clicked settings');
   document.getElementsByClassName('settings-icon')[0].classList.add('settings-active');
   currentState.textContent = "settings";
   console.log('settings active');
   document.getElementsByClassName('brain')[0].classList.add('brain-section-hide');
   console.log('brain section hide');
-})
-
+});
 
 export { activateSettings };

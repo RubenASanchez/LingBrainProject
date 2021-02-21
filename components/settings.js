a33555ebd813fbac9ef1635f26681b1b0433fa70
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-21 10:44:37 -0800

    settings: add remove class

--- components/settings.js
+++ components/settings.js
@@ -1,13 +1,15 @@
 let currentState = document.getElementById('current-state');
-let settingsButton = document.getElementById('settings-button')
+let settingsButton = document.getElementById('settings-button');
+let settingsIcon = document.getElementsByClassName('settings-icon')[0];
 
 let activateSettings = settingsButton.addEventListener('click',function() {
   if (currentState.textContent === "settings") {
+    settingsIcon.classList.remove('settings-active');
     console.log('settings already active');
   }
   else {
     //console.log('clicked settings');
-    document.getElementsByClassName('settings-icon')[0].classList.add('settings-active');
+    settingsIcon.classList.add('settings-active');
     currentState.textContent = "settings";
     console.log('settings active');
     hideAllElements();

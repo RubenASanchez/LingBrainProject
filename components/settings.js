da33036915f8b92440d6c56a3e80ab88d0890dae
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-21 10:13:00 -0800

    settings: compotenize brain function

--- components/settings.js
+++ components/settings.js
@@ -6,18 +6,17 @@ let activateSettings = settingsButton.addEventListener('click',function() {
     console.log('settings already active');
   }
   else {
-    console.log('clicked settings');
+    //console.log('clicked settings');
     document.getElementsByClassName('settings-icon')[0].classList.add('settings-active');
     currentState.textContent = "settings";
     console.log('settings active');
-    document.getElementsByClassName('brain')[0].classList.add('brain-section-hide');
-    //console.log('brain section hide');
-    moveBrain();
+    hideBrain();
   }
 });
 
-function moveBrain() {
-  console.log('move brain');
+function hideBrain() {
+  document.getElementsByClassName('brain')[0].classList.add('brain-section-hide');
+  console.log('hide brain');
 }
 
 export { activateSettings };

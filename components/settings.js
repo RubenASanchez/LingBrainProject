6119fe700356f70dae5274189766b4345744e524
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-02-27 14:34:37 -0800

    settings: current state

--- components/settings.js
+++ components/settings.js
@@ -55,9 +55,11 @@ let activateTheme = toggleTheme.addEventListener('click',function() {
 
 function stateManagement(currentState) {
   if (currentState.textContent === "settings") {
+    console.log(currentState)
     console.log(`settings active ${currentState}`);
   }
   else {
+    console.log(currentState)
     console.log(`settings not active ${currentState}`);
   }
 }

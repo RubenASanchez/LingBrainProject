f581c229ff4528493ce28314ef2ae90e9007832c
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-03-01 14:03:27 -0800

    statemachine: test add import

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { brainRegionsObject } from '../data/dataset.js';
-//import { activateSettings } from '../components/settings.js';
+import { activateSettings } from '../components/settings.js';
 //import { showInfo } from '../components/fetchdata.js';
 
 if('serviceWorker' in navigator) {

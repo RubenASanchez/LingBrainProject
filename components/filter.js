e13227c2cbbf72e72557cf5fc3dc8b8848d7e680
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-03-01 14:40:16 -0800

    statemachine: update filter state

--- components/filter.js
+++ components/filter.js
@@ -1,7 +1,11 @@
+import { currentState , previousStateText , updateStateInfo } from '../js/statemachine.js';
+
 let filterButton = document.getElementById('filter-button');
 
 let activateFilter = filterButton.addEventListener('click',function() {
   console.log('clicked filter');
+  currentState.textContent = "filter";
+  updateStateInfo(currentState.textContent);
 });
 
 export { activateFilter };

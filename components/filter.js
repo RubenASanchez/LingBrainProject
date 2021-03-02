05f590c9613e82a6faef059cef7dad8c348f89b9
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-03-02 09:29:12 -0800

    filter: add toggle

--- components/filter.js
+++ components/filter.js
@@ -4,7 +4,7 @@ let filterButton = document.getElementById('filter-button');
 
 let activateFilter = filterButton.addEventListener('click',function() {
   console.log('clicked filter');
-  filterButton.classList.add('filter-active');
+  filterButton.classList.toggle('filter-active');
   currentState.textContent = "filter";
   updateStateInfo(currentState.textContent);
 });

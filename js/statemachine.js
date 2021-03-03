be3bf3bf9520fa002bcfe88f0673cb22c3b2bb7d
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-03-03 15:57:44 -0800

    statemachine: test array

--- js/statemachine.js
+++ js/statemachine.js
@@ -4,22 +4,17 @@ let currentState = document.getElementById('current-state');
 let previousStateText = currentState.textContent;
 
 const stateLog = [];
+let oldState = stateLog[0];
+let newState = stateLog[1];
 
 function updateState(emittedState) {
   stateLog.push(emittedState);
+  console.log(`old state is ${oldState}`);
+  console.log(`new state is ${newState}`);
   if (stateLog.length > 2) {
     stateLog.shift();
   }
 }
-
-updateState(currentState);
-console.log(stateLog);
-
-let oldState = stateLog[0];
-let newState = stateLog[1];
-
-console.log(`old state is ${oldState}`);
-console.log(`new state is ${newState}`)
 
 
 

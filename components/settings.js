623b2f6fff9fcab08eeed46a682eb081fc2a8914
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-03-04 17:21:46 -0800

    settings: add hide filter

--- components/settings.js
+++ components/settings.js
@@ -58,6 +58,7 @@ let activateTheme = toggleTheme.addEventListener('click',function() {
 function hideAllElements() {
   document.getElementsByClassName('brain')[0].classList.add('section-hide');
   //console.log('hide all non settings elements');
+  filterButton.classList.add('settings-inactive');
 }
 
 function showPreviousElement() {

3c54246dbe3920f64d8a987ae73c0374e698761d
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-03-26 15:31:10 -0700

    statemachine: inactivate all previous components

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
-import { brainRegionsObject } from '../data/dataset.js';
-import { activateReady } from '../components/regions.js';
-import { activateFilter } from '../components/filter.js';
-import { activateSettings } from '../components/settings.js';
+//import { brainRegionsObject } from '../data/dataset.js';
+// import { activateReady } from '../components/regions.js';
+// import { activateFilter } from '../components/filter.js';
+// import { activateSettings } from '../components/settings.js';
 
-if('serviceWorker' in navigator) {
-  console.log('browser can use sw');
-  navigator.serviceWorker.register('sw.js').then(registration => {
-    console.log('sw register successful',registration);
-  })
-  .catch(error => {
-    console.log('sw register error',error);
-  });
-}
+
+
+// SW
+
+// if('serviceWorker' in navigator) {
+//   console.log('browser can use sw');
+//   navigator.serviceWorker.register('sw.js').then(registration => {
+//     console.log('sw register successful',registration);
+//   })
+//   .catch(error => {
+//     console.log('sw register error',error);
+//   });
+// }
+
+
+
+
 
 // console.log('region info card index brainRegions 0 name');
 
-const name = brainRegionsObject.brainRegions[0].name;
-const namecard = document.getElementById('name');
-namecard.textContent = name;
+// const name = brainRegionsObject.brainRegions[0].name;
+// const namecard = document.getElementById('name');
+// namecard.textContent = name;
 
-const location = brainRegionsObject.brainRegions[0].location;
-const locationcard = document.getElementById('location');
-locationcard.textContent = location;
+// const location = brainRegionsObject.brainRegions[0].location;
+// const locationcard = document.getElementById('location');
+// locationcard.textContent = location;
 
-const functions = brainRegionsObject.brainRegions[0].function;
-const functioncard = document.getElementById('function');
-functioncard.textContent = functions;
+// const functions = brainRegionsObject.brainRegions[0].function;
+// const functioncard = document.getElementById('function');
+// functioncard.textContent = functions;
 
 // file to import all js files into
 //let requestURL = 'https://researchscientist.github.io/LingBrainProject/data/dataset.json'
--- js/statemachine.js
+++ js/statemachine.js
@@ -1,17 +1,12 @@
 //import { activateSettings } from '../components/settings.js';
 
-let currentState = document.getElementById('current-state');
-let previousStateText = currentState.textContent;
+let stateDisplay = document.getElementById('current-state');
+let currentTextContent = stateDisplay.textContent;
 
-const stateLog = ['ready',currentState.textContent];
-
-
+const stateLog = ['ready'];
 
 
-
-
-
-
+// const myArray = ['brain']; now called stateLog
 
 
 // NEW STATE MACHINE BEGIN
@@ -37,16 +32,14 @@ const stateLog = ['ready',currentState.textContent];
 
 // display text
 
-// let stateDisplay = document.getElementById('state-display');
-// let currentTextContent = stateDisplay.textContent;
-// const myArray = ['brain'];
 
 // settings
 
-// const settingsButton = document.getElementById('settings-button');
-// let settingsEmmit = settingsButton.addEventListener('click',function() {
+const settingsButton = document.getElementById('settings-button');
+let settingsEmmit = settingsButton.addEventListener('click',function() {
+  console.log('settings clicked')
 //   sm('settings');
-// });
+});
 
 // filter
 
@@ -164,43 +157,38 @@ const stateLog = ['ready',currentState.textContent];
 
 
 
-var SMcurrentState = currentState.textContent;
-var SMpreviousState;
+// var SMcurrentState = currentState.textContent;
+// var SMpreviousState;
 
-function stateMachine(newEmmitedState) {
-  //SMcurrentState = currentState.textContent;
-  SMpreviousState = SMcurrentState;
-  SMcurrentState = newEmmitedState;
-  console.log(`sm previous state ${SMpreviousState}`);
-  console.log(`sm current state ${SMcurrentState}`);
-};
-
-
-var oldState;
-var newState;
-
-function updateState(emittedState) {
-  console.log(`initial log is ${stateLog}`)
-  stateLog.push(emittedState);
-  console.log(`after push log is ${stateLog}`)
-  while (stateLog.length > 2) {
-    stateLog.shift();
-  }
-  oldState = stateLog[0];
-  newState = stateLog[1];
-  console.log(`after shift log is ${stateLog}`)
-  console.log(`old state is ${oldState}`);
-  console.log(`new state is ${newState}`);
-  return oldState,newState;
-}
-
-// this does not return the updated values
-//console.log(`pulled out old is ${oldState} and new is ${newState}`);
-// need to make a function to handle delivering old and new states
+// function stateMachine(newEmmitedState) {
+//   //SMcurrentState = currentState.textContent;
+//   SMpreviousState = SMcurrentState;
+//   SMcurrentState = newEmmitedState;
+//   console.log(`sm previous state ${SMpreviousState}`);
+//   console.log(`sm current state ${SMcurrentState}`);
+// };
 
 
-function updateStateInfo(stateText) {
-  console.log(`the updated state is currently ${stateText}`);
-}
+// var oldState;
+// var newState;
 
-export { currentState , previousStateText , updateStateInfo , updateState, oldState , newState , stateMachine };
+// function updateState(emittedState) {
+//   console.log(`initial log is ${stateLog}`)
+//   stateLog.push(emittedState);
+//   console.log(`after push log is ${stateLog}`)
+//   while (stateLog.length > 2) {
+//     stateLog.shift();
+//   }
+//   oldState = stateLog[0];
+//   newState = stateLog[1];
+//   console.log(`after shift log is ${stateLog}`)
+//   console.log(`old state is ${oldState}`);
+//   console.log(`new state is ${newState}`);
+//   return oldState,newState;
+// }
+
+// function updateStateInfo(stateText) {
+//   console.log(`the updated state is currently ${stateText}`);
+// }
+
+//export { stateDisplay , updateStateInfo , updateState, oldState , newState , stateMachine };

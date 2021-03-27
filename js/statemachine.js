9373b83fab331ecf68cef92e555538c0eee412c1
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2021-03-27 11:57:49 -0700

    statemachien: implement statemachine logic for processing received button signals

--- js/statemachine.js
+++ js/statemachine.js
@@ -43,7 +43,7 @@ let filterEmmit = filterButton.addEventListener('click',function() {
 // STATE MACHINE RECEIVES EMITTED SIGNAL & UPDATES CURRENT STATE
 
 function sm(emitted) {
-  //console.log(`someone emitted ${emitted}`);
+  //console.log(`someone emitted ${emitted}`); 
   if (stateLog[stateLog.length-1] == emitted) {
     stateLog.pop();
     console.log(stateLog);
